723bdfc5c231d20d9d0c2e59f11c1afeb44e2ecc
TooSalty <50435165+TooSalty0000@users.noreply.github.com>
2023-09-16 08:12:03 +0900

    First Init

---
+++ noFreeRider/src/main/java/com/teamnine/noFreeRider/NoFreeRiderApplication.java
@@ -0,0 +1,13 @@
+package com.teamnine.noFreeRider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoFreeRiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoFreeRiderApplication.class, args);
+	}
+
+}
