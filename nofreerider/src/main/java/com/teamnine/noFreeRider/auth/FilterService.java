e19db1702d438b6c2809e9f7380a7c898bfa3db5
TooSalty <50435165+TooSalty0000@users.noreply.github.com>
2023-11-11 17:59:16 +0900

    filter

--- nofreerider/src/main/java/com/teamnine/noFreeRider/auth/FilterService.java
+++ nofreerider/src/main/java/com/teamnine/noFreeRider/auth/FilterService.java
@@ -1,5 +1,7 @@
 package com.teamnine.noFreeRider.auth;
 
+import com.teamnine.noFreeRider.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,9 +10,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Service
 public class FilterService extends OncePerRequestFilter {
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+//        final MemberRepository memberRepository;
+//
+//
+//        if (request.getRequestURI().equals("/login")) {
+//           if (response.)
+//        } else {
+//            filterChain.doFilter(request, response);
+//        }
     }
 }
