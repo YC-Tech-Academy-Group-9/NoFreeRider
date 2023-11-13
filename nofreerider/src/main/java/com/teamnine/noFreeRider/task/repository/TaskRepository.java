3af45a16a49dc2361672efdee04b3707b47cf6cb
apuhapu <laks852@naver.com>
2023-11-13 23:56:39 +0900

    프로젝트에 귀속된 테스크 조회 기능 구현

--- nofreerider/src/main/java/com/teamnine/noFreeRider/task/repository/TaskRepository.java
+++ nofreerider/src/main/java/com/teamnine/noFreeRider/task/repository/TaskRepository.java
@@ -4,8 +4,12 @@ import com.teamnine.noFreeRider.task.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    public List<Task> findAllByProjectId(UUID projectId);
+
 }
