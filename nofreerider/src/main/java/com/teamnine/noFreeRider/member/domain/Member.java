61e9c5af5e29368561af5b07e9e28a8fbf465e44
TooSalty <12saltyh@gmail.com>
2023-11-18 15:10:14 +0900

    Member connect with main page

--- nofreerider/src/main/java/com/teamnine/noFreeRider/member/domain/Member.java
+++ nofreerider/src/main/java/com/teamnine/noFreeRider/member/domain/Member.java
@@ -1,8 +1,6 @@
 package com.teamnine.noFreeRider.member.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -49,14 +47,14 @@ public class Member implements UserDetails {
     private LocalDateTime created_at;
 
     @Column(name = "member_temperature")
-    private short member_temperature;
+    private short memberTemperature;
 
     public Member(String memberName, String member_email, int member_studentId, String member_major) {
         this.memberName = memberName;
         this.memberEmail = member_email;
         this.memberStudentId = member_studentId;
         this.memberMajor = member_major;
-        this.member_temperature = 36;
+        this.memberTemperature = 36;
     }
 
     @Override
