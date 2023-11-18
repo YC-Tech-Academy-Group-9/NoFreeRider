2f367ad7f0081ad67e3949efa47a69b0797bf4b6
minisemin <minisemin@yonsei.ac.kr>
2023-11-18 17:13:19 +0900

    signup initializing member information working

--- nofreerider/src/main/java/com/teamnine/noFreeRider/member/service/MemberService.java
+++ nofreerider/src/main/java/com/teamnine/noFreeRider/member/service/MemberService.java
@@ -80,4 +80,13 @@ public class MemberService {
 
         return tokenInfo;
     }
+
+    public Member getMemberByEmail(String memberEmail) {
+
+        return memberRepository.findByMemberEmail(memberEmail).orElseThrow(() -> new IllegalArgumentException("not found member"));
+    }
+
+    public String getCurrentMemberEmailFromToken(String token) {
+        return jwtTokenProvider.getUserEmailFromToken(token);
+    }
 }
